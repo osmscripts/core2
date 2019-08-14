6bfab04cecd6e84355db18f9804bc08269f1e80a
Vladislav Osmianskij <vlaosm@gmail.com>
2019-08-14 08:29:45 +0300

    Creating `osmscripts` script in progress; using `pimple/pimple`

---
+++ src/Script.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace OsmScripts\Core;
+
+use Symfony\Component\Console\Application;
+
+class Script
+{
+    public function __construct($name) {
+    }
+
+    public function run() {
+
+    }
+}
